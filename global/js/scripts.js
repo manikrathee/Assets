242a519971345431c2dd790db1318ffe376b4c2b
Manik Rathee <manikrathee@gmail.com>
2013-04-29 21:50:43 -0700

    added new JS libs, new script.js file and concatenated imports

---
+++ global/js/scripts.js
@@ -0,0 +1,31 @@
+/*
+ * scripts.js
+ *
+ * auto-concatenated jquery, symbolset and custom alternate files
+ *
+ */
+
+//@codekit-prepend "libs/jquery-1.7.1.min.js";
+//@codekit-prepend "libs/lazyload.js";
+//@codekit-prepend "libs/picturefill.js";
+//@codekit-prepend "libs/fittext.js";
+//@codekit-prepend "libs/lettering.js";
+
+var win   = $(window);
+var doc   = $(document);
+var body  = $('body');
+var $self = $(this);
+
+
+/*  Google Analytics  */
+var googleAnalyticsID = "UA-19400273-5";
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', googleAnalyticsID]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
