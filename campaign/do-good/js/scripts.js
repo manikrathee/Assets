10b40415b285253cd99f24bd1af9ea9f72070282
Manik Rathee <manikrathee@gmail.com>
2013-08-03 19:02:06 -0700

    added campaign/do-good

---
+++ campaign/do-good/js/scripts.js
@@ -0,0 +1,25 @@
+/*
+ * scripts.js
+ *
+ * auto-concatenated jquery, symbolset and custom alternate files
+ *
+ */
+
+//@codekit-prepend "libs/jquery-2.0.js"";
+
+var win   = $(window);
+var doc   = $(document);
+var body  = $('body');
+var $self = $(this);
+
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-40787829-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
