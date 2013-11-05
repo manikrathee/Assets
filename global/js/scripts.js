922dda7f77c0c1be4ffa847254096d75a37d2a52
Manik Rathee <manikrathee@gmail.com>
2013-11-05 09:16:39 -0800

    optimized images, added fitbid

--- global/js/scripts.js
+++ global/js/scripts.js
@@ -10,22 +10,4 @@
 //@codekit-prepend "libs/picturefill.js";
 //@codekit-prepend "libs/fittext.js";
 //@codekit-prepend "libs/lettering.js";
-
-var win   = $(window);
-var doc   = $(document);
-var body  = $('body');
-var $self = $(this);
-
-
-/*  Google Analytics  */
-var googleAnalyticsID = "UA-";
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', googleAnalyticsID]);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+//@codekit-prepend "libs/fitvids.js";
